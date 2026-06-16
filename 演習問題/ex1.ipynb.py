7ffd3d830a2813dbd71611a32e361c858e153a7c
uenomakoto-9807 <makoto9807@gmail.com>
2026-06-16 15:24:44 +0900

    演習問題１を追加しました

--- 演習問題/ex1.ipynb.py
+++ 演習問題/ex1.ipynb.py
@@ -1,2 +1,12 @@
-test=(input('成績')
-      
+test=int(input('成績:'))
+if test < 0 or test > 100:
+     print('入力エラーです。')
+
+elif test>=80:
+    print(f'{test}点は優です。')
+elif test>=70:
+    print(f'{test}点は良です。')
+elif test>=60:
+    print(f'{test}点は可です。')
+else:
+    print(f'{test}点は不可です。')
