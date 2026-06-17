3a1dfac177e56e6dae9a28f5bf10df5a45f3e5a6
uenomakoto-9807 <makoto9807@gmail.com>
2026-06-17 10:16:39 +0900

    演習問題5を追加しました

---
+++ 演習問題/ex5.py
@@ -0,0 +1,28 @@
+def pfact (n):
+    print(f"素因数分解:{n}=",end="")
+    x=2
+    while n%x!=0:
+        x+=1
+    print(f"{x}",end="")
+    n//=x
+    while n%2==0:
+        print(f"*{2}",end="")
+        n//=2
+
+    f=3
+    while f*f<=n:
+        if n%f==0:
+            print(f"*{n}",end="")
+            n//=f
+        else:
+            f+=2
+    if n!=1:
+        print(f"*{n}",end="")
+
+    
+        
+a=int(input("正の整数:"))
+if a<2:
+    a=int(input("正の整数:"))
+pfact(a)
+    
