7021f22287602571101069d648d8705a9d1fa2a4
uenomakoto-9807 <makoto9807@gmail.com>
2026-06-19 14:47:51 +0900

    演習７を追加しました

---
+++ 演習問題/ex7.1.py
@@ -0,0 +1,18 @@
+y=input("整数型データを入力してください。\n").split()
+a=[]
+for x in y:
+  a.append(int(x))
+print(f"{a}")
+print(type(a[1]))
+
+max=a[0]
+min=a[0]
+for x in a:
+    if max < x:
+        max=x
+for x in a:
+    if min > x:
+        min=x
+print(f"最大値は{max}、最小値は{min}です。")
+               
+               
---
+++ 演習問題/ex7.py
@@ -0,0 +1,12 @@
+data = [int(x) for x in input("整数型データを入力してください。\n").split()]
+
+max=data[0]
+min=data[0]
+for x in data:
+    if max < x:
+        max=x
+for x in data:
+    if min > x:
+        min=x
+print(f"最大値は{max}、最小値は{min}です。")
+               
